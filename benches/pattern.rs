6cb8c9504f0aff314adae026c593761ee5fafbe1
unknowntrojan <14975032+unknowntrojan@users.noreply.github.com>
2022-09-09 13:34:29 +0200

    Initial commit.

---
+++ benches/pattern.rs
@@ -0,0 +1,55 @@
+#![feature(test)]
+
+use findpattern::{find_pattern, find_pattern_par, pattern};
+use rand::Rng;
+use test::Bencher;
+
+extern crate test;
+
+#[bench]
+fn bench_pattern_1gig(b: &mut Bencher) {
+    let mut rng = rand::thread_rng();
+
+    let size: usize = 1024 * 1024 * 1024;
+    let mut test_pattern: Vec<u8> = (0..size).map(|_| rng.gen_range(0..=255)).collect();
+
+    // let pattern = size / 2;
+    let pattern = size - 5;
+
+    test_pattern[pattern] = 0xDE;
+    test_pattern[pattern + 1] = 0xAD;
+    test_pattern[pattern + 2] = 0xFF;
+    test_pattern[pattern + 3] = 0xBE;
+    test_pattern[pattern + 4] = 0xEF;
+
+    b.iter(|| {
+        assert_eq!(
+            find_pattern(&test_pattern, &pattern!(0xDE, 0xAD, _, 0xBE, 0xEF)),
+            Some(pattern)
+        );
+    });
+}
+
+#[bench]
+fn bench_pattern_parallel_1gig(b: &mut Bencher) {
+    let mut rng = rand::thread_rng();
+
+    let size: usize = 1024 * 1024 * 1024;
+    let mut test_pattern: Vec<u8> = (0..size).map(|_| rng.gen_range(0..=255)).collect();
+
+    // let pattern = size / 2;
+    let pattern = size - 5;
+
+    test_pattern[pattern] = 0xDE;
+    test_pattern[pattern + 1] = 0xAD;
+    test_pattern[pattern + 2] = 0xFF;
+    test_pattern[pattern + 3] = 0xBE;
+    test_pattern[pattern + 4] = 0xEF;
+
+    b.iter(|| {
+        assert_eq!(
+            find_pattern_par(&test_pattern, &pattern!(0xDE, 0xAD, _, 0xBE, 0xEF)),
+            Some(pattern)
+        );
+    });
+}
---
+++ src/lib.rs
@@ -0,0 +1,105 @@
+#![feature(core_intrinsics)]
+//!
+//! This crate provides a naive implementation of a pattern finding algorithm.
+//!
+//! It also provides a macro to create the data structure required, `pattern`.
+//! This crate isn't recommended to be used as a stand-alone, but wrapped by my `signature` crate.
+//!
+
+#[cfg(feature = "parallel")]
+use rayon::{prelude::IndexedParallelIterator, slice::ParallelSlice};
+
+pub type Pattern = Vec<Option<u8>>;
+
+/// Creates a pattern.
+///
+/// Internally, this is a `[Option<u8>]`.
+///
+/// `None`'s, represented here as `_`, match anything.
+///
+/// `Some`'s match their content directly.
+///
+/// # Examples
+/// ```
+/// # use findpattern::pattern;
+/// let pat = pattern!(0xE9, _, _, _, _);
+/// dbg!(pat); // {Some(0xE9), None, None, None, None}
+/// ```
+#[macro_export]
+macro_rules! pattern {
+    ($($elem:tt),+) => {
+        vec![$(pattern!(@el $elem)),+]
+    };
+    (@el $v:expr) => {
+        Some($v)
+    };
+    (@el $v:tt) => {
+        None
+    };
+}
+
+#[inline(always)]
+fn match_pattern(window: &[u8], pattern: &Pattern) -> bool {
+    window.iter().zip(pattern).all(|(v, p)| match p {
+        Some(x) => *v == *x,
+        None => true,
+    })
+}
+
+///
+/// Returns the first position within `region` that matches `pattern`.
+///
+/// Short-circuiting.
+///
+pub fn find_pattern(region: &[u8], pattern: &Pattern) -> Option<usize> {
+    region
+        .windows(pattern.len())
+        .position(|wnd| core::intrinsics::unlikely(match_pattern(wnd, pattern)))
+}
+
+#[cfg(feature = "parallel")]
+pub fn find_pattern_par(region: &[u8], pattern: &Pattern) -> Option<usize> {
+    region
+        .par_windows(pattern.len())
+        .position_any(|wnd| core::intrinsics::unlikely(match_pattern(wnd, pattern)))
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn pattern_fwd() {
+        let test_pattern: [u8; 32] = [
+            0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+            0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0xDE, 0xAD, 0xFF, 0xBE, 0xEF, 0x00, 0x00, 0x00,
+            0x00, 0x00, 0x00, 0x00,
+        ];
+
+        let pattern = pattern!(0xDE, 0xAD, ?, 0xBE, 0xEF);
+
+        assert_eq!(find_pattern(&test_pattern, &pattern), Some(20));
+    }
+
+    #[cfg(feature = "parallel")]
+    #[test]
+    fn pattern_fwd_par() {
+        let test_pattern: [u8; 32] = [
+            0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+            0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0xDE, 0xAD, 0xFF, 0xBE, 0xEF, 0x00, 0x00, 0x00,
+            0x00, 0x00, 0x00, 0x00,
+        ];
+
+        let pattern = pattern!(0xDE, 0xAD, ?, 0xBE, 0xEF);
+
+        assert_eq!(find_pattern_par(&test_pattern, &pattern), Some(20));
+    }
+
+    #[test]
+    fn matching() {
+        assert!(match_pattern(
+            &[0xDE, 0xAD, 0x00, 0xBE, 0xEF],
+            &pattern!(0xDE, 0xAD, ?, 0xBE, 0xEF)
+        ))
+    }
+}
